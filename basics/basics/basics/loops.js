9525e745c15de18447812a1fde8ca666fdf39991
A16YTO <hughrendorfabila@gmail.com>
2025-08-07 21:06:46 +0800

    Update loops.js
    learning

--- basics/basics/basics/loops.js
+++ basics/basics/basics/loops.js
@@ -1,1 +1,18 @@
+// For loop: count from 1 to 5
+for (let i = 1; i <= 5; i++) {
+  console.log("Count: " + i);
+}
+
+// While loop: countdown from 5 to 1
+let countdown = 5;
+while (countdown > 0) {
+  console.log("Countdown: " + countdown);
+  countdown--;
+}
+
+// For...of loop: list hobbies
+let hobbies = ["coding", "reading", "gaming"];
+for (let hobby of hobbies) {
+  console.log("Hobby: " + hobby);
+}
 
