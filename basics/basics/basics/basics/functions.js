536d0506bc6b6368329c415a3e670a061870b8be
A16YTO <hughrendorfabila@gmail.com>
2025-08-07 21:08:59 +0800

    Update functions.js

--- basics/basics/basics/basics/functions.js
+++ basics/basics/basics/basics/functions.js
@@ -1,1 +1,27 @@
+// Function declaration
+function greet(name) {
+  console.log(`Hello, ${name}!`);
+}
+
+greet("Hugh");
+
+// Function with return value
+function calculateArea(radius) {
+  const PI = 3.14159;
+  return PI * radius * radius;
+}
+
+let area = calculateArea(5);
+console.log(`Area of circle: ${area}`);
+
+// Function expression
+const isEven = function (num) {
+  return num % 2 === 0;
+};
+
+console.log("Is 4 even?", isEven(4));
+
+// Arrow function
+const square = (x) => x * x;
+console.log("Square of 6:", square(6));
 
